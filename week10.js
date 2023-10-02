c2a94db96ff25c10db1d8b072fc3705efe243db1
Milton Aineruhanga <milton_ug@yahoo.com>
2023-10-01 22:04:37 -0700

    week10CodingProject

---
+++ week10.js
@@ -0,0 +1,15 @@
+let id=0;
+// Create event listener to push the form data to the table when the submit button is clicked
+document.getElementById('report').addEventListener('click', () => {
+    let table = document.getElementById('records');
+    let row = table.insertRow();
+    row.setAttribute('id', `item-${id}`);
+    row.insertCell(0).innerHTML = document.getElementById('new-location').value
+    row.insertCell(1).innerHTML = `$ ${document.getElementById('new-price').value}`
+    row.insertCell(2).innerHTML = document.getElementById('new-datetime').value
+    
+// Clear the form after submission
+    document.getElementById('new-location').value = '';
+    document.getElementById('new-price').value = '';
+    document.getElementById('new-datetime').value = '';
+});
