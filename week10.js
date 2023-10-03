c51c8174e6edbd1fe1c5c00d511c80532531cabf
Milton Aineruhanga <milton_ug@yahoo.com>
2023-10-02 20:58:38 -0700

    Removed unused code. All else works without it

--- week10.js
+++ week10.js
@@ -1,15 +1,17 @@
-let id=0;
+let id=1;
 // Create event listener to push the form data to the table when the submit button is clicked
 document.getElementById('report').addEventListener('click', () => {
     let table = document.getElementById('records');
     let row = table.insertRow();
     row.setAttribute('id', `item-${id}`);
-    row.insertCell(0).innerHTML = document.getElementById('new-location').value
-    row.insertCell(1).innerHTML = `$ ${document.getElementById('new-price').value}`
-    row.insertCell(2).innerHTML = document.getElementById('new-datetime').value
+    row.insertCell(0).innerHTML = id
+    row.insertCell(1).innerHTML = document.getElementById('new-location').value
+    row.insertCell(2).innerHTML = `$ ${document.getElementById('new-price').value}`
+    row.insertCell(3).innerHTML = document.getElementById('new-datetime').value
     
 // Clear the form after submission
     document.getElementById('new-location').value = '';
     document.getElementById('new-price').value = '';
     document.getElementById('new-datetime').value = '';
+    id++;
 });
